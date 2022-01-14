08f788548272a2dbf21015b6753225aa4ce32b93
Vikas Jyani <vickbishnoi@gmail.com>
2022-01-14 18:59:59 +0530

    almost ready to push to API

--- index.js
+++ index.js
@@ -12,3 +12,16 @@ fetch("https://apis.scrimba.com/jsonplaceholder/posts")
     }
     document.getElementById("blog-list").innerHTML = html;
   });
+
+document
+  .getElementById("new-post")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    const postTitle = document.getElementById("post-title").value;
+    const postBody = document.getElementById("post-body").value;
+    const data = {
+      title: postTitle,
+      body: postBody,
+    };
+    console.log(data);
+  });
